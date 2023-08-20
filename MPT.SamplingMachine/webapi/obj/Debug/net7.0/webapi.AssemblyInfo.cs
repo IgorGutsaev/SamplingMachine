a7b4972d44e86bbda443d99e25fa470adfabc0ec
Igor Gutsaev <igor.gutsaev@filuet.com>
2023-08-20 16:35:48 +0300

    launch fix

--- MPT.SamplingMachine/webapi/obj/Debug/net7.0/webapi.AssemblyInfo.cs
+++ MPT.SamplingMachine/webapi/obj/Debug/net7.0/webapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d40276bba4cb617421c63bf14ca652532a1b921d")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
